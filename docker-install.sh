6c412255ad3051f71c6b96030329e05dc56fd382
alesc <github@alesc.fr>
2018-11-21 10:22:43 +0100

    Unpublish mysql and apply patch mysql 5.1.46

--- docker-install.sh
+++ docker-install.sh
@@ -66,7 +66,7 @@ fi
 tar -xzf guacamole-auth-jdbc-${GUACVERSION}.tar.gz
 
 # Start MySQL
-docker run --restart=always --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=$mysqlrootpassword" --publish 3306:3306 mysql
+docker run --restart=always --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=$mysqlrootpassword" mysql
 
 # Sleep to let MySQL load (there's probably a better way to do this)
 echo "Waiting 30 seconds for MySQL to load"
@@ -78,14 +78,26 @@ SQLCODE="
 create database guacamole_db; 
 create user 'guacamole_user'@'%' identified by '$guacdbuserpassword'; 
 GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'%'; 
+create user 'guacamole_user'@'localhost' identified by '$guacdbuserpassword';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
 flush privileges;"
 
 # Execute SQL Code
-echo $SQLCODE | mysql -h 127.0.0.1 -P 3306 -u root -p$mysqlrootpassword
+echo $SQLCODE |  docker exec -i mysql mysql -p$mysqlrootpassword
 
-cat guacamole-auth-jdbc-${GUACVERSION}/mysql/schema/*.sql | mysql -u root -p$mysqlrootpassword -h 127.0.0.1 -P 3306 guacamole_db
+cat guacamole-auth-jdbc-${GUACVERSION}/mysql/schema/*.sql | docker exec -i mysql mysql -p$mysqlrootpassword guacamole_db
 
 docker run --restart=always --name guacd -d guacamole/guacd
 docker run --restart=always --name guacamole  --link mysql:mysql --link guacd:guacd -e MYSQL_HOSTNAME=127.0.0.1 -e MYSQL_DATABASE=guacamole_db -e MYSQL_USER=guacamole_user -e MYSQL_PASSWORD=$guacdbuserpassword --detach -p 8080:8080 guacamole/guacamole
 
 rm -rf guacamole-auth-jdbc-${GUACVERSION}*
+
+# Patch for latest mysql version compatibility
+docker exec -it guacamole bash -c "
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz
+tar xf mysql-connector-java-5.1.46.tar.gz
+rm mysql-connector-java-5.1.46.tar.gz
+mv mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /opt/guacamole/mysql/
+rm /opt/guacamole/mysql/mysql-connector-java-5.1.35-bin.jar
+"
+docker restart guacamole
